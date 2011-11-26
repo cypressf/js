96439d81040de5cca2f818056f380d8af5980b43
Cypress Frankenfeld <iamcypress@gmail.com>
2011-11-26 18:38:42 -0500

    wrote some compatibility functions
    added functions:
    addClass: a method of Element.prototype that adds a class to the element
    removeClass: a method of Element.prototype that removes a class from
    the element
    addListener: a method of Node.prototype that adds an event listener
    removeListener: a method of Node.prototype that removes an event
    listener
    
    I will work on these functions in the future to make sure that they
    behave properly in all modern browsers. Right now, they probably don't.

--- js.js
+++ js.js
@@ -3,8 +3,6 @@ TODO:
 -organize the giant closure and return pointers to any methods and variables that should be public
 
 -look at compatibility
---addEventListener --> attachEvent
---if neither addEventListener or attachEvent exist, element.onevent = function
 --is stopPropagation the best way to do it? I see {window.event.cancelBubble = true; window.event.returnValue false} or {event.stopPropagation(); event.preventDefault()}
 
 -simplify and trim! goal: cut out 40 lines of code
@@ -24,34 +22,39 @@ TODO:
 
 var closure = function(){
     var debug = true;
-    var num = 0;
     var moveables = [];
     
-    /* todo: only run init when the document is ready */
-    init();
-    
-    function write(string) {
-        if(debug) { console.log(string); }
-    }
-    function addEvent(element, eventName, callback) {
-        if( element === null ) { return "no such element"; }
-        if( element.addEventListener ) {
-            element.addEventListener(eventName,callback,false);
-            return "added callback " + callback + " to event " + eventName;
+    /* todo: make addClass and removeClass compatible in all browsers
+    right now they only work in browsers that support classList */
+    Element.prototype.addClass = function(classname){
+        this.classList.add(classname);
+    };
+    Element.prototype.removeClass = function(classname){
+        this.classList.remove(classname);
+    };
+    Node.prototype.addListener = function(eventType, listener, useCapture){
+        if( this.addEventListener ) { /* modern browser */
+            this.addEventListener(eventType, listener, useCapture);
         }
-        else if( element.attachEvent ) {
-            element.attachEvent("on" + eventName, callback);
-            return "added callback " + callback + " to event " + eventName + " (IE sucks, by the way)";
+        else if( element.attachEvent ) { /* ie */
+            this.attachEvent("on" + eventType, listener);
         }
-    }
-    function removeEvent(element, eventName, callback) {
-        if(element === null) { return; }
-        if(element.removeEventListener) {
-            element.removeEventListener(eventName, callback, false);
+        else { /* old browser */
+            this["on" + eventType] = listener;
+        }
+    };
+    Node.prototype.removeListener = function(eventType, listener, useCapture){
+        if(this.removeEventListener) {
+            this.removeEventListener(eventType, listener, useCapture);
         }
         else if(element.detachEvent) {
-            element.detachEvent("on" + eventName, callback);
+            this.detachEvent("on" + eventType, listener);
         }
+        /* todo: is there another case where the browser doesn't
+            support detachEvent? */
+    };
+    function write(string) {
+        if(debug) { console.log(string); }
     }
     function makeElement(type, id, classes, parent) {
         var element = document.createElement(type);
@@ -62,43 +65,58 @@ var closure = function(){
         parent.appendChild(element);
         return element;
     }
+    
+    /* todo: UX: make it so you can drag from one side to the other in a different direction*/
     function newMoveable(event){
-        moveables = moveables.concat(new moveable("moveable" + num,[event.pageX-20,event.pageY-20],[0,0]));
-        var handle = moveables[num].handles[6];
-        moveables[num].update();
+        var position = [event.pageX-20,event.pageY-20];
+        var size = [0,0];
+        var mv = new moveable(position, size);
+        moveables = moveables.concat( mv );
+        var handle = mv.handles[6];
+        mv.update();
         handle.mouseInit = [event.pageX, event.pageY];
         handle.sizeInit = [handle.container.size[0], handle.container.size[1]];
         handle.positionInit = [handle.container.position[0], handle.container.position[1]];
-        moveables[num].deleter.classList.add("hidden");
-        document.body.classList.add("bottom");
-        document.body.classList.add("right");
-        addEvent(document, "mousemove", resizeMoveable);
-        addEvent(document, "mouseup", endNewMoveable);
+        
+        mv.deleter.addClass("hidden");
+        document.body.addClass("bottom");
+        document.body.addClass("right");
+        
+        document.addListener("mousemove", resizeMoveable, false);
+        document.addListener("mouseup", endNewMoveable, false);
+        
+        /*todo: there is a better way than to nest these functions
+        inside newMoveable...*/
+        function endNewMoveable(event){
+            mv.deleter.removeClass("hidden");
+            document.body.removeClass("bottom");
+            document.body.removeClass("right");
+            document.removeListener("mousemove", resizeMoveable, false);
+            document.removeListener("mouseup", endNewMoveable, false);
+            write("done creating");
+        }
+        
+        function resizeMoveable(event){
+            mv.handles[6].drag(event);
+        }
     }
-    function endNewMoveable(event){
-        moveables[num].deleter.classList.remove("hidden");
-        document.body.classList.remove("bottom");
-        document.body.classList.remove("right");
-        removeEvent(document, "mousemove", resizeMoveable);
-        removeEvent(document, "mouseup", endNewMoveable);
-        num += 1;
-    }
-    function resizeMoveable(event){
-        moveables[num].handles[6].drag(event);
-    }
+
+
     function removeMoveable(moveable){
         write("moveable" + moveable.id + " was removed");
         document.body.removeChild(moveable.element);
+        /* todo: make sure the element and the moveable object
+        are deleted from memory. leaks are bad.*/
+        moveable.element = null; // does this help?
         moveables.pop(moveable);
-        num -= 1;
     }
     
     /* moveable */
-    function moveable(id,position,size){
-        write("creating moveable: " + id);
-        this.id = id;
+    function moveable(position,size){
+        this.id = moveables.length + 1;
+        write("creating moveable: " + this.id);
         var that = this;
-        this.element = makeElement("div",id,["block"],document.body);
+        this.element = makeElement("div",this.id,["block"],document.body);
         this.position = position;
         this.size = size;
         this.maxposition = [10000, 10000];
@@ -123,8 +141,7 @@ var closure = function(){
         var deleteLink = document.createElement("a");
         this.deleter = this.element.appendChild(deleteLink);
         this.deleter.innerHTML = "x";
-        this.deleter.setAttribute("href","#delete" + id);
-        this.deleter.draggable = false;
+        this.deleter.setAttribute("href","#delete" + this.id);
 
 
     this.createHandles = function() {
@@ -152,8 +169,8 @@ var closure = function(){
             document.body.classList.add("grabbing");
             that.mouseDiff = [that.position[0] - event.pageX, that.position[1] - event.pageY];
             write("beginMove");
-            addEvent(document, 'mousemove', that.move);
-            addEvent(document, 'mouseup', that.endMove);
+            document.addListener('mousemove', that.move, false);
+            document.addListener('mouseup', that.endMove, false);
         };
 
         this.move = function(event){
@@ -164,8 +181,8 @@ var closure = function(){
         this.endMove = function(event){
             write('endMove');
             document.body.classList.remove("grabbing");
-            removeEvent(document, 'mousemove', that.move);
-            removeEvent(document, 'mouseup', that.endMove);
+            document.removeListener('mousemove', that.move, false);
+            document.removeListener('mouseup', that.endMove, false);
         };
 
         this.setValue = function(possize, value, xy){
@@ -176,25 +193,25 @@ var closure = function(){
 
         this.startRemove = function(event){
             write("starting remove");
-            event.stopPropagation();
-            addEvent(document, 'mouseup', that.cancelRemove);
-            addEvent(that.deleter, 'mouseup', that.remove);
+            event.stopPropagation(); /*todo: this may not always work. fix the compatibility issue*/
+            document.addListener('mouseup', that.cancelRemove, false);
+            that.deleter.addListener('mouseup', that.remove, false);
         };
 
         this.remove = function(event){
             removeMoveable(that);
-            removeEvent(document, "mouseup", that.cancelRemove);
-            removeEvent(that.deleter, "mouseup", that.remove);        
+            document.removeListener('mouseup', that.cancelRemove, false);
+            that.deleter.removeListener('mouseup', that.remove, false);        
         };
 
         this.cancelRemove = function(event){
             write('remove canceled');
-            removeEvent(document, "mouseup", that.cancelRemove);
-            removeEvent(that.deleter, "mouseup", that.remove);
+            document.removeListener('mouseup', that.cancelRemove, false);
+            that.deleter.removeListener('mouseup', that.remove, false);
         };
 
-        addEvent(this.element, "mousedown", this.beginMove);
-        addEvent(this.deleter, "mousedown", this.startRemove);
+        this.element.addListener("mousedown", this.beginMove, false);
+        this.deleter.addListener("mousedown", this.startRemove, false);
     }
 
     /* resizer */
@@ -208,8 +225,8 @@ var closure = function(){
             that.mouseInit = [event.pageX, event.pageY];
             that.sizeInit = [that.container.size[0], that.container.size[1]];
             that.positionInit = [that.container.position[0], that.container.position[1]];
-            addEvent(document, 'mousemove', that.drag);
-            addEvent(document, 'mouseup', that.endDrag);
+            document.addListener('mousemove', that.drag, false);
+            document.addListener('mouseup', that.endDrag, false);
             for(var i in that.sides){
                 document.body.classList.add(that.sides[i]);
             }
@@ -225,8 +242,8 @@ var closure = function(){
         };
 
         this.endDrag = function(event){
-            removeEvent(document, 'mousemove', that.drag);
-            removeEvent(document, 'mouseup', that.endDrag);
+            document.removeListener('mousemove', that.drag, false);
+            document.removeListener('mouseup', that.endDrag, false);
             for(var i in that.sides){
                 document.body.classList.remove(that.sides[i]);
             }
@@ -251,14 +268,26 @@ var closure = function(){
             that.container.setValue("size",that.mouse[1] - that.mouseInit[1] + that.sizeInit[1],1);
         };
 
-        addEvent(this.element, "mousedown", this.beginDrag);
+        this.element.addListener("mousedown", this.beginDrag, false);
+    }
+    
+    function getMoveables(){
+        return moveables;
+    }
+    
+    function setDebug(bool){
+        debug = bool;
     }
     
     function init(){
         write("starting");
-        write(addEvent(document,"mousedown",newMoveable));
+        document.addListener("mousedown",newMoveable, false);
     }
-    
+    /* todo: only run init when the document is ready */
+    init();
     /* todo: return pointers to public methods or variables */
-    return {};
+    return {
+        mv: moveables,
+        debug: debug
+    };
 }();
